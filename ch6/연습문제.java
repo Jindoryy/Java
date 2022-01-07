da0d2d4cdfdad154c575d34e26d7c857d5088989
진규 <87755660+Jindoryy@users.noreply.github.com>
2022-01-07 17:12:25 +0900

    Update 연습문제.java

--- ch6/연습문제.java
+++ ch6/연습문제.java
@@ -1,4 +1,4 @@
-
+# 6-19 check
 
 
 
@@ -133,3 +133,54 @@ class Student {
 }
 
 # 6-6
+
+public class Hello {
+	public static void main(String[] args) {
+
+		System.out.println(getDistance(1,1,2,2));
+	}
+	
+	static double getDistance(int x, int y, int x1, int y1) {
+		return Math.sqrt((x - x1) * (x - x1) + (y - y1) * (y - y1));
+	}
+}
+
+# 6-7
+	
+class MyPoint {
+	int x;
+	int y;
+	
+	MyPoint(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	double getDistance(int x1, int y1) {
+		return Math.sqrt((x - x1) * (x - x1) + (y - y1) * (y - y1));
+	}
+}
+
+public class Hello {
+	public static void main(String[] args) {
+
+		MyPoint p = new MyPoint(1, 1);
+		
+		System.out.println(p.getDistance(2, 2));
+	}
+}
+
+# 6-8
+생략
+
+# 6-9
+생략
+
+# 6-10 ~ 6-17 
+단순 문제
+
+# 6-18
+생략
+
+# 6-19
+다시한번 볼 필요
